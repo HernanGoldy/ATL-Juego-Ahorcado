29b595e3147e37c4364d58b52c53aa62e0ac0e05
HernanGoldy <hernanmdq@gmail.com>
2023-08-15 06:29:57 -0300

    Inicio del proyecto Juego Ahorcado

---
+++ src/main/java/com/example/AtlJuegoAhorcadoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlJuegoAhorcadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtlJuegoAhorcadoApplication.class, args);
+    }
+
+}
